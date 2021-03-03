ef3a7a531d6d1bbd8906a2e1c4c176d9d7976da6
753879 <78134499+753879@users.noreply.github.com>
2021-03-03 19:27:57 +0900

    Add files via upload

---
+++ Coupon/src/main/java/forthcafe/Coupon.java
@@ -0,0 +1,97 @@
+package forthcafe;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Coupon_table")
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String ordererName;
+    private String menuName;
+    private Long menuId;
+    private Double price;
+    private Integer quantity;
+    private String status;
+    private Integer piece;
+
+    @PrePersist
+    public void onPrePersist(){
+        CouponSaved couponSaved = new CouponSaved();
+        BeanUtils.copyProperties(this, couponSaved);
+        couponSaved.publishAfterCommit();
+
+
+        CouponCancelled couponCancelled = new CouponCancelled();
+        BeanUtils.copyProperties(this, couponCancelled);
+        couponCancelled.publishAfterCommit();
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getOrdererName() {
+        return ordererName;
+    }
+
+    public void setOrdererName(String ordererName) {
+        this.ordererName = ordererName;
+    }
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+    public Long getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(Long menuId) {
+        this.menuId = menuId;
+    }
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public Integer getPiece() {
+        return piece;
+    }
+
+    public void setPiece(Integer piece) {
+        this.piece = piece;
+    }
+
+
+
+
+}
---
+++ Coupon/src/main/java/forthcafe/CouponCancelled.java
@@ -0,0 +1,23 @@
+
+package forthcafe;
+
+public class CouponCancelled extends AbstractEvent {
+
+    private Long id;
+    private Integer piece;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Integer getPiece() {
+        return piece;
+    }
+
+    public void setPiece(Integer piece) {
+        this.piece = piece;
+    }
+}
